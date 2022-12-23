2f6ce043f67d49efa8bb0102ec2c0936e199f98f
Vishwa Chikate <vishwa.chikate@gmail.com>
2022-12-23 17:14:35 +0530

    Repository remove JS code, support for PHP8.2

--- src/php/Helpers/Node.php
+++ src/php/Helpers/Node.php
@@ -3,6 +3,10 @@
 namespace LeetCode\Problems\Helpers;
 
 class Node {
+    public $val = null;
+    public $left = null;
+    public $right = null;
+    public $next = null;
     function __construct($val = 0) {
         $this->val = $val;
         $this->left = null;
