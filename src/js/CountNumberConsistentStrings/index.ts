4c74a5cfe7881ee8d10ff799cf227969d82ee038
vishwac09 <vishwa.chikate@gmail.com>
2024-09-12 08:48:15 +0530

    Problem 12-9-2024

--- src/js/CountNumberConsistentStrings/index.ts
+++ src/js/CountNumberConsistentStrings/index.ts
@@ -2,7 +2,7 @@ export const countConsistentStrings = (allowed: string, words: string[]): number
   const setOfAlowedString = new Set(allowed);
   let count = words.length;
   words.forEach((word) => {
-    for (let i=0; i<word.length; i++) {
+    for (let i = 0; i < word.length; i++) {
       if (!setOfAlowedString.has(word[i])) {
         count--;
         break;
